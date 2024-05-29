e7b54d92af060d42f400d55e061a188738e8a019
Crowdbotics <team@crowdbotics.com>
2024-05-29 10:38:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tralalion-ai-48152",
+  url: "https://tralalion-ai-48152.botics.co",
+  api: "https://tralalion-ai-48152.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
